5229c4a252fe9da498286af45b236a82c803c749
Hyunwoong Kang <hyunwoong.kang@leedotype.com>
2022-02-09 22:32:56 +0900

    gave up true color. now shows color

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -3,9 +3,6 @@ use crate::utils;
 use std::io;
 use termion::{clear, color, style};
 
-type Bg = color::Bg<color::Rgb>;
-type Fg = color::Fg<color::Rgb>;
-
 #[derive(Copy, Clone, Eq, PartialEq)]
 pub enum State {
     Correct,
@@ -15,16 +12,6 @@ pub enum State {
 }
 
 impl State {
-    pub fn get_color(&self) -> (Bg, Fg) {
-        let bg = match self {
-            State::Correct => color::Bg(color::Rgb(83, 141, 78)),
-            State::Present => color::Bg(color::Rgb(181, 159, 59)),
-            State::Absent => color::Bg(color::Rgb(58, 58, 60)),
-            State::Unused => color::Bg(color::Rgb(129, 131, 132)),
-        };
-        (bg, color::Fg(color::Rgb(215, 218, 220)))
-    }
-
     pub fn get_block(&self) -> char {
         match self {
             State::Correct => '🟩',
@@ -43,8 +30,13 @@ pub struct Tile {
 
 impl Tile {
     pub fn print(&self) {
-        let (bg, fg) = self.guess.get_color();
-        print!("{}{}{}", bg, fg, self.letter);
+        let fg = color::Fg(color::LightWhite);
+        match self.guess {
+            State::Correct => print!("{}{}{}", color::Bg(color::Green), fg, self.letter),
+            State::Present => print!("{}{}{}", color::Bg(color::Yellow), fg, self.letter),
+            State::Absent => print!("{}{}{}", color::Bg(color::LightBlack), fg, self.letter),
+            State::Unused => print!("{}{}{}", color::Bg(color::Black), fg, self.letter),
+        }
     }
 }
 
